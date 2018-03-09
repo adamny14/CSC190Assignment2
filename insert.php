0532b4dcb0111a3280d4b2595914a749880572ce
Jacob Hochendoner <hochen97@gmail.com>
2018-03-08 19:15:51 -0500

    added MySQL config

--- insert.php
+++ insert.php
@@ -1,3 +1,15 @@
 <?php
+/*----------MySQL Connection----------*/
+$server = 'localhost';
+$user = 'root';
+$pass = 'goodyear123!@#';
+$dbname = 'List';
+$con = mysql_connect($server, $user, $pass) or die("Can't connect");
+mysql_select_db($dbname);
+/*------------------------------------*/
+
+$nickname = $_POST['nickname'];
+$location = $_POST['location'];
+
 
 ?>
